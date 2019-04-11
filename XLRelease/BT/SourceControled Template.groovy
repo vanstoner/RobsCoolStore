93d5e5abda88e8c6b1bafc7a516a61bff9653b50
Rob Vanstone <rvanstone@xebialabs.com>
2019-04-11 16:48:14 +0100

    Initial Create

---
+++ XLRelease/BT/SourceControled Template.groovy
@@ -0,0 +1,16 @@
+// Exported from:        http://Robs-MBP-2:5516/#/templates/Foldere98fe4726d2140c1b6985f012e6a25c3-Released21633f471954c04a9272c83c01db96a/releasefile
+// XL Release version:   8.6.1
+// Date created:         Thu Apr 11 16:48:12 IST 2019
+
+xlr {
+  template('SourceControled Template') {
+    folder('BT')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2019-04-11T09:00:00+0100')
+    phases {
+      phase('New Phase') {
+        
+      }
+    }
+    
+  }
+}
